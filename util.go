33594940d39f81110996f5a79eccf3763bce4ecf
Dan Belwood <dan.belwood@gmail.com>
2015-09-20 15:27:14 -0500

    Rename some funcs and add CreateResult helper

--- util.go
+++ util.go
@@ -10,15 +10,15 @@ func CheckError(err error) {
 	}
 }
 
-func pipeline(in <-chan Record, fns []func(Record) Record) <-chan Record {
+func Pipeline(in <-chan Record, fns []func(Record) Record) <-chan Record {
 	c := in
 	for _, fn := range fns {
-		c = pipeFunction(c, fn)
+		c = StreamFunction(c, fn)
 	}
 	return c
 }
 
-func pipeFunction(in <-chan Record, fn func(Record) Record) <-chan Record {
+func StreamFunction(in <-chan Record, fn func(Record) Record) <-chan Record {
 	out := make(chan Record)
 	go func() {
 		for record := range in {
@@ -27,4 +27,20 @@ func pipeFunction(in <-chan Record, fn func(Record) Record) <-chan Record {
 		close(out)
 	}()
 	return out
+}
+
+func CreateResult(success bool, in Record, err error, out *Record) Result {
+	if success {
+		return Result{
+			successful: true,
+			input:      in,
+			output:     &out,
+		}
+	} else {
+		return Result{
+			successful: false,
+			input:      in,
+			error:      err,
+		}
+	}
 }
